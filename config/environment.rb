d203fe0bf7828466b5f97e7d75711d59cd0a23a5
Gunnar Magholder <gunnarmagholder@Macintosh.local>
2008-11-02 23:06:36 +0100

    Changed Rails Release to 2.1.2

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,7 @@ ActiveEnv = 'development'
 
 ENV['RAILS_ENV'] ||= ActiveEnv
 
-RAILS_GEM_VERSION = '2.1.1' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.1.2' unless defined? RAILS_GEM_VERSION
 HOST = case ActiveEnv
   when 'development' then 'http://127.0.0.1:3000'
   when 'test'        then 'http://127.0.0.1'
