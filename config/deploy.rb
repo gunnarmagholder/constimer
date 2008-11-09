7c00483195318f1065d7274b2395f241c39631ca
Gunnar Magholder <gunnarmagholder@Macintosh.local>
2008-11-09 21:47:10 +0100

    Deployment pains

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ set :repository,  "git@github.com:gunnarmagholder/constimer.git"
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-set :deploy_to, "/var/www/#{application}"
+set :deploy_to, "/var/www/apps/#{application}"
 
 set :scm, :git 
 set :domain, "v30136.1blu.de"
@@ -13,20 +13,8 @@ role :app, domain
 role :web, domain
 role :db,  domain, :primary => true
 
+set :runner, user
 
-default_run_options[:pty] = true
-set :user, "deploy"
-set :ssh_options, { :forward_agent => true }
-set :use_sudo, false
-
-namespace :deploy do
-  desc "upload database configuration"
-  task :upload_database_configuration, :roles => :app do
-    run "mkdir -p #{shared_path}/config"
-    config = File.read(File.join(File.dirname(__FILE__), "database.yml"))
-    put config, "#{shared_path}/config/database.yml"
-    run "ln -nfs #{shared_path}/config/database.yml #{latest_release}/config/database.yml"
-  end
-  
-  after "deploy:finalize_update", "deploy:upload_database_configuration"
-end
+set :mongrel_servers, 3
+set :mongrel_port, 8000
+set :rails_env, 'production'
