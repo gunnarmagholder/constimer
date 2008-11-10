126e0387fadf250f14da045376419c5ac8fe1f9a
Gunnar Magholder <gunnarmagholder@Macintosh.local>
2008-11-10 18:09:46 +0100

    Uploading to home dir

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,21 @@
 set :application, "www.road-timer.com"
 set :user, "deploy"
-set :repository,  "git@github.com:gunnarmagholder/constimer.git"
+
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-set :deploy_to, "/var/www/apps/#{application}"
-
+set :deploy_to, "/home/#{user}/apps/#{application}"
+set :deploy_via, :remote_cache
 set :scm, :git 
+set :repository,  "git@github.com:gunnarmagholder/constimer.git"
+set :branch, "master"
+
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
+set :scm_verbose, true
+
+
 set :domain, "v30136.1blu.de"
 role :app, domain
 role :web, domain
