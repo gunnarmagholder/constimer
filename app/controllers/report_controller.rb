f5bae28468954f3be8d415618332594e9dec40ca
Gunnar Magholder <gunnarmagholder@Macintosh.local>
2008-11-17 19:24:17 +0100

    Started Reportingfunctions

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,5 +1,6 @@
 class ReportController < ApplicationController
   def index
     @user = current_user
+    @entries = Entry.find(:all, :conditions => ['user_id = ? ', current_user.id ], :order => ['project_id, edate ASC'])
   end
 end
