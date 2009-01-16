6f0217e97de8acfa21f1bfd91a81e863fa960a57
Gunnar Magholder <gunnarmagholder@Macintosh.local>
2009-01-16 20:45:19 +0100

    Private Projekte implementiert

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -3,9 +3,9 @@ class ReportController < ApplicationController
     @user = current_user
     if current_user.isRole('manager')
       @colleagues = User.find(:all, :conditions => {:managed_by => current_user.id})
-      conditions = ['user_id in (?) ', @colleagues]
+      conditions = ['`projects`.`private` <> true AND `entries`.`user_id` in (?) ', @colleagues]
     else
-      conditions = ['user_id = ?', current_user.id]
+      conditions = ['`entries`.`user_id` = ?', current_user.id]
     end
     if params[:project]
       if params[:project][:id] != ""
@@ -24,14 +24,14 @@ class ReportController < ApplicationController
       end
     end
     
-    @entries = Entry.find(:all, :conditions => conditions)
+    @entries = Entry.find(:all, :include => :project, :conditions => conditions)
     @overall_min = @entries.sum(&:minutes)
     if current_user.isRole('manager') 
-	  	@projects = Project.find(:all, :conditions => ['user_id = ? OR user_id in (?)', current_user.id, @colleagues])
+	  	@projects = Project.find(:all, :conditions => ['(user_id = ? OR user_id in (?)) AND private = 0', current_user.id, @colleagues])
     else
       @projects = Project.find(:all, :conditions => ['user_id = ?', current_user.id])
     end
-    @years = Entry.find(:all, :group => 'year(edate)',:conditions => ['user_id = ?', current_user.id])
+    @years = Entry.find(:all, :group => 'year(edate)',:conditions => ['`entries`.`user_id` = ?', current_user.id])
   end
   
   def show
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,9 +1,15 @@
 class Project < ActiveRecord::Base
   belongs_to :user
   has_many :entries
+  before_create :set_private
   
   def minutes
     Entry.sum(:minutes, :conditions => ['project_id = ?', self.id])
   end
+  
+  private
+    def set_private
+      self.private = 0 unless self.private == 1
+    end
   
 end
