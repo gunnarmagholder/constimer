5716b4840339bd180561f936d685d3400f6655be
Gunnar Magholder <gunnarmagholder@Macintosh.local>
2009-02-06 22:37:22 +0100

    Searching for the failure in the host

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
-    if current_user
+    if logged_in?
       if current_user.isRole('user')
         @assignment_request = Assignment.find_by_user_and_status(current_user.id, 1)
         @entries = Entry.find(:all, :conditions => ['user_id = ? and endtime IS NULL', current_user.id ])
