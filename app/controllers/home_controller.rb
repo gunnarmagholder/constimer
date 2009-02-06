d10229dd32287637c6394ed3dcdc07add29008d6
Gunnar Magholder <gunnarmagholder@Macintosh.local>
2009-02-06 23:13:35 +0100

    deleted the open entry list

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,8 @@
 class HomeController < ApplicationController
   def index
-    @entries = []
     if logged_in?
       if current_user.isRole('user')
         @assignment_request = Assignment.find_by_user_and_status(current_user.id, 1)
-        @entries = Entry.find(:all, :conditions => ['user_id = ? and endtime IS NULL', current_user.id ])
       end
     end
   end
