51f5c012ec627bb6ed3b5a73cde1ca8524f8d058
Gunnar Magholder <gunnarmagholder@Macintosh.local>
2009-02-06 23:49:53 +0100

    Dummenerror

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,6 +4,7 @@ class HomeController < ApplicationController
       if current_user.isRole('user')
         @assignment_request = Assignment.find_by_user_and_status(current_user.id, 1)
       end
+      @entries = Entry.find(:all, :conditions => ['user_id = ? and endtime IS NULL', current_user.id])
     end
   end
 
